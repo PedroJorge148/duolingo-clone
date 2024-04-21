78fba06ee3c508219252934c9e39412ca3b783cc
Pedro Jorge <pedrojorge2011@gmail.com>
2024-04-21 18:02:47 -0300

    feat: add stripe setup

--- src/app/(main)/shop/items.tsx
+++ src/app/(main)/shop/items.tsx
@@ -6,6 +6,7 @@ import { useTransition } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { refillHearts } from '@/actions/user-progress'
+import { createStripeUrl } from '@/actions/user-subscription'
 
 interface ItemsProps {
   hearts: number
@@ -25,6 +26,18 @@ export function Items({ hearts, points, hasActiveSubscription }: ItemsProps) {
 
     startTransition(() => {
       refillHearts().catch(() => toast.error('Something went wrong'))
+    })
+  }
+
+  function onUpgrade() {
+    startTransition(() => {
+      createStripeUrl()
+        .then((response) => {
+          if (response.data) {
+            window.location.href = response.data
+          }
+        })
+        .catch(() => toast.error('Something went wrong'))
     })
   }
 
@@ -49,6 +62,17 @@ export function Items({ hearts, points, hasActiveSubscription }: ItemsProps) {
               <p>{POINTS_TO_REFILL}</p>
             </div>
           )}
+        </Button>
+      </div>
+      <div className="flex w-full items-center gap-4 border-t-2 p-4 pt-8">
+        <Image src="/unlimited.svg" alt="Unlimited" width={60} height={60} />
+        <div className="flex-1">
+          <p className="text-base font-bold text-neutral-700 lg:text-lg">
+            Unlimited Hearts
+          </p>
+        </div>
+        <Button onClick={onUpgrade} disabled={pending || hasActiveSubscription}>
+          {hasActiveSubscription ? 'active' : 'upgrade'}
         </Button>
       </div>
     </ul>
--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,9 @@ import { z } from 'zod'
 const envSchema = z.object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
+  STRIPE_API_KEY: z.string(),
   DATABASE_URL: z.string(),
+  STRIPE_WEBHOOK_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
